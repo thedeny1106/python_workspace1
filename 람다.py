8230eaa2464e1622e77e545892324605f4c78675
littleconan@hanmail.net <littleconan@hanmail.net>
2025-04-29 17:49:30 +0900

    0429

--- 람다.py
+++ 람다.py
@@ -63,3 +63,74 @@ findList = ( list( filter(lambda e:e["name"]==keyname, personList)) )
 print(findList) 
 
 #40세 이상만 
+findList = ( list( filter(lambda e:e["age"]>=40, personList)) )
+print(findList) 
+
+#map, sort :정렬, zip:다른언어에 없음 
+#map - 연산을 수행한다.  나이-5
+#첫번째 매개변수가 매개변수 하나 값 하나를 반환하는 함수이어야 한다  
+for i in map(lambda x: x*10 , a):
+    print(i)
+
+def myfunc2(x):
+     x["age"] = x["age"] +5
+     return x
+
+for per in map(myfunc2, personList):
+    print(per)    
+
+#데이터정렬    list타입 자체가 sort메서드가 있다 
+# sorted 함수가 filter처럼 별도로 존재한다 
+# key 라는 매개변수에 람다를 전달해야 한다
+a=[9,4,5,6,7,8,1,2,10,3]
+a.sort() #내 내부 데이터가 정렬된다. 
+print(a) 
+#dict 타입은 < 연산자가 없다. 에러발생
+#람다는 매개변수 하나고 반환값이 > 연산자가 지원되는 반환타입만 가능하다. 
+#오름차순이 기본값이다 
+personList.sort(key = lambda x : x["name"]) #정렬을 할때  a[0] < a[1]
+print(personList) 
+
+personList.sort(key = lambda x : x["name"], reverse=True) #reverse값이 False로 되어있다
+print(personList) 
+
+#list타입에 속한 함수 sort는 자신의 순서가 바뀐다. 
+#sorted(iterable데이터, key, reverse) 첫번째 매개변수로 전달된 데이터 원래 순서는 안바꾸고 
+#바뀐 순서를 list로 만들어서 전달한다 
+
+a=[9,4,5,6,7,8,1,2,10,3]
+b = sorted(a)
+print("a=", a)
+print("b=", b)
+
+personList = [
+    {"name":'홍길동',  "age":34, "phone":"010-0000-0001"},
+    {"name":'강감찬',  "age":70, "phone":"010-0000-0004"},
+    {"name":'서희',    "age":54, "phone":"010-0000-0003"},
+    {"name":'윤관',    "age":39, "phone":"010-0000-0002"},
+    {"name":'김종서',  "age":38, "phone":"010-0000-0005"},
+    {"name":'이순신',  "age":44, "phone":"010-0000-0006"},
+    {"name":'곽재우',  "age":62, "phone":"010-0000-0009"}
+]
+
+list2 = sorted(personList, key=lambda per:per["name"])
+print(list2)
+
+
+
+#오늘의 과제 
+#words = ["assembly", "java", "rain", "notebook", "north", 
+#            "south", "hospital", "programming", "house", "hour"]
+#문제1. filter를 사용해서 글자수가 6글자 이상인 단어만 출력하기 (컴프리핸션X)
+#문제2. map함수를 사용해서 글자를 대문자로 바꾸어서 출력하기  (컴프리핸션X)
+#문제3. sorted 함수를 사용하여 단어들의 길이순으로 오름차순 정렬하여 출력하기
+#문제4. sorted 함수를 사용하여 알파벳 순으로 내림차순으로 정렬하여 출력하기 
+#문제5. 단어중에 o가 포함되는 단어가 모두 몇개인지 카운트하기 (힌트,filter를 사용)        
+
+
+
+
+
+
+
+
