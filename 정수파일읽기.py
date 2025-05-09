ee4096954b7d2d089a43b02811856c261ae55707
littleconan@hanmail.net <littleconan@hanmail.net>
2025-05-09 12:53:01 +0900

    0508

--- 정수파일읽기.py
+++ 정수파일읽기.py
@@ -1,5 +1,13 @@
 
 f = open("정수.txt", "r")
 lines = f.readlines() 
-print(lines)
+sum=0
+for line in lines:
+    if "\n" in line:
+        line = line[:len(line)-1]
+    print(line)    
+    sum += int(line)
 f.close()
+
+print("평균", sum/len(lines))
+
