738cc00b21f21b83cab6c665ce3ce08663ccceeb
littleconan@hanmail.net <littleconan@hanmail.net>
2025-05-12 10:10:10 +0900

    '0512'

---
+++ python_package_example/test_script.py
@@ -0,0 +1,7 @@
+from mypackage import math_utils
+from mypackage.submodule import string_utils
+
+print(math_utils.add(2, 3))
+print(math_utils.subtract(5, 2))
+print(string_utils.to_upper("hello"))
+print(string_utils.to_lower("WORLD"))
